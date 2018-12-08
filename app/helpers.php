2f11ec3cef48f1bbd26cea71ad1768073074dfc5
vagrant <vagrant@homestead>
2018-12-08 21:18:16 +0000

    improve view helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,10 @@
 <?php
-function view($path)
+function view($path,$params = [])
 {
       $path = str_replace('.','/',$path);
+      foreach ($params as $key => $value){
+            $$key = $value;
+      }
       ob_start();
       include 'resources/views/'.$path.'.blade.php';
       return  ob_get_clean();
